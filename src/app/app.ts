7a177f1a5fd65ace050b693bc48157b6085f5b8d
lucaseduardomeyer2007-creator <lucaseduardomeyer2007@gmail.com>
2026-06-05 18:22:16 -0300

    Curso

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,9 +3,12 @@ import { RouterOutlet, RouterLink } from '@angular/router';
 import {BoasVindas} from './boas-vindas/boas-vindas';
 import {CidadeBlumenau} from './cidade-blumenau/cidade-blumenau';
 import {CidadeIndaial} from './cidade-indaial/cidade-indaial';
+import {Comidas} from './comidas/comidas';
+import {Carros} from './carros/carros';
+import {Filmes} from './filmes/filmes';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, BoasVindas, CidadeBlumenau, CidadeIndaial],
+  imports: [RouterOutlet, RouterLink, BoasVindas, CidadeBlumenau, CidadeIndaial, Comidas, Carros, Filmes],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
